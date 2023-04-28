a1fb52cd6ed8cb6a905ecdc3acb1975db89cfaff
cenajhonakash@gmail.com <cenajhonakash@gmail.com>
2023-04-28 10:30:27 +0530

    intital commit

---
+++ src/main/java/com/testportal/quizbatchservice/QuizbatchserviceApplication.java
@@ -0,0 +1,13 @@
+package com.testportal.quizbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbatchserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbatchserviceApplication.class, args);
+	}
+
+}
